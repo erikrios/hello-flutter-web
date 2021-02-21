f0673b1fe0e256c6e8514cdf4de9e42ba80390c7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-21 19:06:42 +0700

    Import sign_up_app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'screens/sign_up_app.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello Flutter Web',
-      color: Colors.green,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          title: Text(
-            'Flutter Web',
-          ),
-        ),
-        body: Container(
-          child: Center(
-            child: Text(
-              'Hello, Flutter Web!',
-              style: TextStyle(
-                fontSize: 50.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(SignUpApp());
